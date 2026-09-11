666f2f909faf2ddb527af49509ccc66c40664a03
Maciej Lamberski <maciej@lamberski.com>
2026-09-11 14:54:28 +0200

    feat: Resolve the TuneIn player with its station logo

--- src/defaults/resolvers.ts
+++ src/defaults/resolvers.ts
@@ -151,6 +151,7 @@ import { telegramIframeEmbedResolver, telegramScriptEmbedResolver } from '../emb
 import { tencentEmbedResolver } from '../embeds/tencent.js'
 import { tiktokBlockquoteEmbedResolver, tiktokIframeEmbedResolver } from '../embeds/tiktok.js'
 import { transistorEmbedResolver } from '../embeds/transistor.js'
+import { tuneinEmbedResolver } from '../embeds/tunein.js'
 import {
   twitterAmpEmbedResolver,
   twitterBlockquoteEmbedResolver,
@@ -302,6 +303,7 @@ const embedResolvers: Array<EmbedResolver> = [
   tiktokBlockquoteEmbedResolver,
   tiktokIframeEmbedResolver,
   transistorEmbedResolver,
+  tuneinEmbedResolver,
   twitterBlockquoteEmbedResolver,
   twitterAmpEmbedResolver,
   twitterSubstackEmbedResolver,
