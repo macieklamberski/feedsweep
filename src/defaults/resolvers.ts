a3a394d5cecfa42588f6c3093a489afa43663fd6
Maciej Lamberski <maciej@lamberski.com>
2026-09-11 14:54:38 +0200

    feat: Resolve the Odnoklassniki video player

--- src/defaults/resolvers.ts
+++ src/defaults/resolvers.ts
@@ -117,6 +117,7 @@ import { mixcloudEmbedResolver } from '../embeds/mixcloud.js'
 import { nicovideoIframeEmbedResolver, nicovideoScriptEmbedResolver } from '../embeds/nicovideo.js'
 import { notecomIframeEmbedResolver } from '../embeds/notecom.js'
 import { nytimesIframeEmbedResolver } from '../embeds/nytimes.js'
+import { odnoklassnikiEmbedResolver } from '../embeds/odnoklassniki.js'
 import { odyseeEmbedResolver } from '../embeds/odysee.js'
 import { omnyEmbedResolver } from '../embeds/omny.js'
 import { padletEmbedResolver } from '../embeds/padlet.js'
@@ -264,6 +265,7 @@ const embedResolvers: Array<EmbedResolver> = [
   nicovideoIframeEmbedResolver,
   notecomIframeEmbedResolver,
   nytimesIframeEmbedResolver,
+  odnoklassnikiEmbedResolver,
   odyseeEmbedResolver,
   omnyEmbedResolver,
   padletEmbedResolver,
