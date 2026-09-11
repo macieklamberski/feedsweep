5693678f7b5e99fb7ddb3c23b431c7498a1e008c
Maciej Lamberski <maciej@lamberski.com>
2026-09-11 14:54:11 +0200

    feat: Resolve the Kindle preview card with its cover

--- src/defaults/resolvers.ts
+++ src/defaults/resolvers.ts
@@ -106,6 +106,7 @@ import {
   jwplayerSetupEmbedResolver,
 } from '../embeds/jwplayer.js'
 import { kalturaIframeEmbedResolver, kalturaScriptEmbedResolver } from '../embeds/kaltura.js'
+import { kindleEmbedResolver } from '../embeds/kindle.js'
 import { libsynEmbedResolver } from '../embeds/libsyn.js'
 import { linkedinEmbedResolver } from '../embeds/linkedin.js'
 import { mailruEmbedResolver } from '../embeds/mailru.js'
@@ -252,6 +253,7 @@ const embedResolvers: Array<EmbedResolver> = [
   jwplayerSetupEmbedResolver,
   kalturaIframeEmbedResolver,
   kalturaScriptEmbedResolver,
+  kindleEmbedResolver,
   libsynEmbedResolver,
   linkedinEmbedResolver,
   mailruEmbedResolver,
