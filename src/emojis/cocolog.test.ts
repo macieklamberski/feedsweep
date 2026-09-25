5eadd4345f4e34f374add089fe4f84d5ebd2026d
Maciej Lamberski <maciej@lamberski.com>
2026-09-26 00:04:59 +0700

    feat: Mark TypePad emoticons

--- src/emojis/cocolog.test.ts
+++ src/emojis/cocolog.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'bun:test'
-import { describeForEachParser, emojiConverters } from '../tests.js'
+import { describeForEachParser, emojiConverters, html } from '../tests.js'
 
 describeForEachParser('cocologEmojiResolver', (parseHtml) => {
   const { transform } = emojiConverters(parseHtml)
@@ -8,6 +8,30 @@ describeForEachParser('cocologEmojiResolver', (parseHtml) => {
     const value = '<p><img src="https://emojies.cocolog-nifty.com/emoticon/shine.gif"></p>'
     const expected =
       '<p><img data-emoji="" src="https://emojies.cocolog-nifty.com/emoticon/shine.gif"></p>'
+
+    expect(await transform(value)).toEqualHtml(expected)
+  })
+
+  it('should mark a TypePad pictogram from another host', async () => {
+    const value = html`
+      <p>
+        <img
+          class="emoticon shine"
+          src="https://static.example.com/.shared/images/emoticon/shine.gif"
+          alt="shine"
+        >
+      </p>
+    `
+    const expected = html`
+      <p>
+        <img
+          data-emoji=""
+          class="emoticon shine"
+          src="https://static.example.com/.shared/images/emoticon/shine.gif"
+          alt="shine"
+        >
+      </p>
+    `
 
     expect(await transform(value)).toEqualHtml(expected)
   })
--- src/emojis/cocolog.ts
+++ src/emojis/cocolog.ts
@@ -4,7 +4,10 @@ import { resolveEmojiImage } from '../utils/emojis.js'
 // Cocolog's pictograms, named in English with no alt. They have no Unicode counterpart to become.
 export const cocologEmojiResolver: EmojiResolver = {
   kind: 'emoji',
-  selector: 'img[src*="emojies.cocolog-nifty.com/emoticon/" i]',
+  selector: [
+    'img[src*="emojies.cocolog-nifty.com/emoticon/" i]', // Cocolog
+    'img[src*="/.shared/images/emoticon/" i]', // TypePad, which Cocolog runs on, from any of its hosts
+  ].join(', '),
   extract: (element) => {
     return resolveEmojiImage(element, { isStrong: true })
   },
